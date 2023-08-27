bac941de048fb8349a481560428c4a01e95aa465
Helder <helderlucau@gmail.com>
2023-08-27 12:23:01 +0300

    restaurant initialized with a name as a string and returns the restaurant's name

--- py/customer.py
+++ py/customer.py
@@ -1,5 +1,22 @@
 class Customer:
+
+    #init method that takes two arguments
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
         pass
+
+    def given_name(self):
+        return self.first_name
+    
+    def set_given_name(self, first_name):
+        self.first_name = first_name
+
+    def family_name(self):
+        return self.last_name 
+    
+    def set_family_name(self, last_name):
+        self.last_name = last_name
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
---
+++ py/restaurant.py
@@ -0,0 +1,8 @@
+class Restaurant:
+
+    # init method that takes one argument
+    def __init__(self, name):
+        self._name = name
+    
+    def name(self):
+        return self._name
