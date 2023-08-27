f0413fa68696cfbead7d2580e0c0022a768cccaa
Helder <helderlucau@gmail.com>
2023-08-27 12:52:39 +0300

    init method initialized with a customer, restaurant and rating args

--- py/customer.py
+++ py/customer.py
@@ -20,3 +20,6 @@ class Customer:
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+    def all():
+        pass
---
+++ py/review.py
@@ -0,0 +1,22 @@
+class Review:
+    def __init__(self, customer, restaurant, rating):
+        self._customer = customer
+        self._restaurant = restaurant
+        self.rating = rating
+
+    # returns the rating for a restaurant
+    def rating(self):  
+        return self.rating  
+    
+    # returns the customer object for that review
+    def customer(self):
+        return self._customer
+    
+    # returns the restaurant object for that given review
+    def restaurant(self):
+        return self._restaurant
+
+    # returns all of the reviews
+    def all(self):
+        pass
+
